69723ef4986a327b50b2ba778c20020c572f07d2
tonif95 <101289506+tonif95@users.noreply.github.com>
2024-01-15 08:50:46 +0100

    Proyecto inicializado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        
+        // Obtenemos la divisa inicial
+        Console.WriteLine("Bienvenido al convertidor de divisas\nSelecciona tu divisa inicial:\n1 - CLP\n2 - ARS\n3 - USD\n4 - EUR\n5 - TRY\n6 - GBP");
+        string initialCurrency = Console.ReadLine().ToUpper();
+        // Obtenemos la divisa que queremos convertir
+        Console.WriteLine("Selecciona la divisa a la que quieres convertir tu dinero:\n1 - CLP\n2 - ARS\n3 - USD\n4 - EUR\n5 - TRY\n6 - GBP");
+        string changeCurrency = Console.ReadLine().ToUpper();
+
+
+
+
+
+
+
+    }
+}
+
