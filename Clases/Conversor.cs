fda2390e8f4af96dbac840a3e224908162cb9512
tonif95 <101289506+tonif95@users.noreply.github.com>
2024-01-16 10:27:35 +0100

    Logica de conversion inicializada

---
+++ Clases/Conversor.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertidordivisas.Clases
+{
+    internal class Conversor
+    {        
+
+        static double ConvertirDivisas(string actual, string convertida, double cantidad) 
+        { 
+            switch (convertida)
+            {
+                case "CLP":
+                    switch (actual)
+                    {
+                        case "ARS":
+                            return cantidad * 0.9;
+                        case "USD":
+                            return cantidad * 0.0011;
+                        case "EUR":
+                            return cantidad * 0.0010;
+                        case "TRY":
+                            return cantidad * 0.033;
+                        case "GBP":
+                            return cantidad * 0.00087;
+                        default:
+                            // Manejar otros casos o lanzar una excepción
+                            throw new ArgumentException("Conversión no soportada para la moneda de origen proporcionada.");
+                            
+                    }
+                case "ARS":
+                    switch (actual)
+                    {
+                        case "CLP":
+                            return cantidad * 0.9;
+                        case "USD":
+                            return cantidad * 0.0011;
+                        case "EUR":
+                            return cantidad * 0.0010;
+                        case "TRY":
+                            return cantidad * 0.033;
+                        case "GBP":
+                            return cantidad * 0.00087;
+                        default:
+                            // Manejar otros casos o lanzar una excepción
+                            throw new ArgumentException("Conversión no soportada para la moneda de origen proporcionada.");
+
+                    }
+                case "USD":
+                    switch (actual)
+                    {
+                        case "ARS":
+                            return cantidad * 0.9;
+                        case "CLP":
+                            return cantidad * 0.0011;
+                        case "EUR":
+                            return cantidad * 0.0010;
+                        case "TRY":
+                            return cantidad * 0.033;
+                        case "GBP":
+                            return cantidad * 0.00087;
+                        default:
+                            // Manejar otros casos o lanzar una excepción
+                            throw new ArgumentException("Conversión no soportada para la moneda de origen proporcionada.");
+
+                    }
+                case "EUR":
+                    switch (actual)
+                    {
+                        case "ARS":
+                            return cantidad * 0.9;
+                        case "USD":
+                            return cantidad * 0.0011;
+                        case "CLP":
+                            return cantidad * 0.0010;
+                        case "TRY":
+                            return cantidad * 0.033;
+                        case "GBP":
+                            return cantidad * 0.00087;
+                        default:
+                            // Manejar otros casos o lanzar una excepción
+                            throw new ArgumentException("Conversión no soportada para la moneda de origen proporcionada.");
+
+                    }
+                case "TRY":
+                    switch (actual)
+                    {
+                        case "ARS":
+                            return cantidad * 0.9;
+                        case "USD":
+                            return cantidad * 0.0011;
+                        case "EUR":
+                            return cantidad * 0.0010;
+                        case "CLP":
+                            return cantidad * 0.033;
+                        case "GBP":
+                            return cantidad * 0.00087;
+                        default:
+                            // Manejar otros casos o lanzar una excepción
+                            throw new ArgumentException("Conversión no soportada para la moneda de origen proporcionada.");
+
+                    }
+                case "GBP":
+                    switch (actual)
+                    {
+                        case "ARS":
+                            return cantidad * 0.9;
+                        case "USD":
+                            return cantidad * 0.0011;
+                        case "EUR":
+                            return cantidad * 0.0010;
+                        case "TRY":
+                            return cantidad * 0.033;
+                        case "CLP":
+                            return cantidad * 0.00087;
+                        default:
+                            // Manejar otros casos o lanzar una excepción
+                            throw new ArgumentException("Conversión no soportada para la moneda de origen proporcionada.");
+
+                    }
+                default:
+                    // Manejar otros casos o lanzar una excepción
+                    throw new ArgumentException("Conversión no soportada para la moneda de destino proporcionada.");
+
+
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ class Program
 
 
 
+
+
     }
 }
 
